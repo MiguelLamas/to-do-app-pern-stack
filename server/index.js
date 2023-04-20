8191275d946b6877ed488e59ac1dd2f24d56fe4b
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-04-20 21:13:51 +0100

    Index Folder for Server Set Up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+
+// middleware
+app.use(express.json()); // giving us access to req.body so we can get JSON Data.
+app.use(cors())
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
