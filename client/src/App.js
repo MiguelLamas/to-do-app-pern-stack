3562da58002600cfa96c7573a708e37eecf49bd5
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-04-22 00:09:45 +0100

    Input Todo Component working and logic connecting client to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+//components
+import InputTodo from './components/InputTodo';
+
 function App() {
   return (
     <>
+    <div className="container">
+    <InputTodo />
+    </div>
       
     </>
   );
---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditTodo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditTodo
