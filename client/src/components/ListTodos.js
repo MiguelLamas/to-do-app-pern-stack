204b06c2d3da0c94a057605765fc4182c749b2e5
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-04-24 23:31:10 +0100

    Built the Delete Button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,8 +1,22 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]); //initially an empty array [] because we want to get all todos
-  
+
+  //delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  //get all todos
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -14,40 +28,45 @@ const ListTodos = () => {
     }
   };
 
-  useEffect (() => {
+  useEffect(() => {
     getTodos();
   }, []);
- 
-  console.log(todos);
 
   return (
-  <>
-  {" "}
-  <table className="table mt-5 text-center">
-    <thead>
-      <tr>
-        <th>Description</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-    {/* <tr>
+    <>
+      {" "}
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
         <td>John</td>
         <td>Doe</td>
         <td>john@example.com</td>
       </tr> */}
-      {todos.map(todo => (
-        <tr>
-          <td>{todo.description}</td>
-          <td>Edit</td>
-          <td>Delete</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-  </>
-  ); 
-}
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={(e) => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-export default ListTodos
+export default ListTodos;
