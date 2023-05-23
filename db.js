0ed59e43d546af66a6b2dcf7871105f9edec5baf
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-23 22:26:44 +0100

    configured database connection

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 // configure how to connect to ur database
 
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const pool = new Pool({
-    user: 'miguellamas',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
-    database: 'perntodo'
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
+
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -15,11 +15,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // giving us access to req.body so we can get JSON Data.
 app.use(cors());
 
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static("./client/build"));
+
 if (process.env.NODE_ENV === "production") {
   //server static content
   //npm run build
-  app.use(express.static(path.join(__dirname, "client/build"))
-}
+  app.use(express.static(path.join(__dirname, "client/build")))
+};
 
 // ROUTES //
 
